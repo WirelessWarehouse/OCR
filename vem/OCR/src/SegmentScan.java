b3753249b13f663652c983fa20e1f06efc3eaf82
WirelessWarehouse <WirelessWarehouse@users.noreply.github.com>
2015-10-04 18:38:17 -0400

    Update SegmentScan.java
    development of Segment Form data structure (Seg_Form)

--- vem/OCR/src/SegmentScan.java
+++ vem/OCR/src/SegmentScan.java
@@ -1,5 +1,21 @@
-
+public class Seg_Form {
+  // this structure formats the data specific to each segment
+  unsigned int ID;
+  unsigned int start_X;
+  unsigned int start_Y;
+  unsigned int leftMost;
+  unsigned int rightMost;
+  unsigned int longestLine; //longest straight path in a given segment  
+  unsigned int currLine // length of current computed pixels in a segment
+  unsigned int end_X;
+  unsigned int end_Y;
+  
+  // don't forget to initialize 
+  
+}
 public class SegmentScan{
+  
+  // data structure for Segment Data (to be stored for lower level processing)
   
   SegmentScan(){
     
